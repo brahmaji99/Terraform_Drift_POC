e47a3cfd5b3f1f9fcef6df6a9e10841d3f9a695a
brahmaji99 <brahma.scm9@gmail.com>
2026-03-22 09:58:34 +0000

    changes for s3 versioning

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,7 +6,12 @@ module "s3" {
   source      = "../../modules/s3"
   bucket_name = "brahmaji-dev-bucket-12345"
 
+ }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = module.s3.bucket_id   # or bucket name output
+
   versioning_configuration {
-    status = "Suspended"   # or Disabled
+    status = "Suspended"
   }
 }
