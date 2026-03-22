f9ea5d867fd6f8240d21ff655038d9a0058de418
brahmaji99 <brahma.scm9@gmail.com>
2026-03-22 19:23:19 +0000

    updated ami id

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 module "ec2" {
   source        = "../../modules/ec2"
-  ami_id        = "ami-0abcdef1234567890"
+  ami_id        = "ami-0f5d1713c9af4fe30"
   instance_type = "t3.small"
 }
