f0a6fdad748f74f868f1bff537f6fa1fef6326a1
brahmaji99 <brahma.scm9@gmail.com>
2026-03-31 14:39:15 +0000

    changes for sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,37 +1,8 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
   tags = {
     Name = "Demo-EC2"
   }
 }
 
-# ✅ Security Group (no inline rules)
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  revoke_rules_on_delete = true
-  description = "EC2 security group"
-}
-
-# ✅ SSH Rule
-resource "aws_security_group_rule" "ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/16"]
-  security_group_id = aws_security_group.ec2_sg.id
-}
-
-# ✅ Outbound Rule
-resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_sg.id
-}
