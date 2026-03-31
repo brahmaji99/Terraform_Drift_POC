aba9528336426dcebbe0768e9600268c394c6151
brahmaji99 <brahma.scm9@gmail.com>
2026-03-31 14:13:32 +0000

    added changes for sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,8 +1,28 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   tags = {
     Name = "Demo-EC2"
+  }
+}
+
+ resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "EC2 security group"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  # restricted
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
